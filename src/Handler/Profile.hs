0e9f1eb3e7c525fed344309ef71519f7db504dd5
Saxon Jensen <saxon.jensen@healthengine.com.au>
2019-01-25 17:25:43 +0800

    Linking objetives model, and starting the profile/edit page.

--- src/Handler/Profile.hs
+++ src/Handler/Profile.hs
@@ -6,10 +6,20 @@
 module Handler.Profile where
 
 import Import
+import Data.Maybe (fromJust)
 
 getProfileR :: Handler Html
 getProfileR = do
     (_, user) <- requireAuthPair
+
+    objectives <- runDB $ selectList [] [Asc ObjectiveId, Asc ObjectiveTeamId]
+    teams <- runDB $ selectList [] [Asc TeamId]
+    results <- runDB $ selectList [] [Asc ResultId]
+
+    let groupToTeam = \team obj -> (entityKey team) == (objectiveTeamId $ entityVal obj)
+    let groupResults obj = (obj, rs) where rs = [r | r <- results, (resultObjectiveId $ entityVal r) == (entityKey obj)]
+    let paired = [(team, map groupResults os) | team <- teams, let os = filter (groupToTeam team) objectives]
+
     defaultLayout $ do
         setTitle . toHtml $ userIdent user <> "'s User page"
         $(widgetFile "profile")
