d553d844b28477927015fcc0cfa519ef87b60cfd
Saxon Jensen <saxon.jensen@healthengine.com.au>
2019-06-07 17:05:01 +0800

    Team member admin stuff

--- src/Handler/Teams.hs
+++ src/Handler/Teams.hs
@@ -6,7 +6,11 @@
 {-# LANGUAGE QuasiQuotes #-}
 module Handler.Teams where
 
+import Database.Persist.Sql (fromSqlKey)
 import Import
+import Data.Maybe (fromMaybe)
+import Yesod.Form.Bootstrap3
+import qualified Data.List as L
 
 getTeamsR :: Handler Value
 getTeamsR = do
@@ -23,7 +27,11 @@ getTeamR :: TeamId -> Handler Html
 getTeamR teamId = do
     (userId, user) <- requireAuthPair
     team <- runDB $ get404 teamId
-    let teamAdmin = userAdmin
+
+    isMember <- isMemberOf userId teamId
+    let canWrite = userAdmin user && isMember
+
+    members <- runDB $ selectList [TeamMemberTeamId ==. teamId] []
 
     defaultLayout $ do
         $(widgetFile "team")
@@ -39,3 +47,22 @@ postTeamObjectivesR team = do
     objectiveId <- runDB $ insert objective
     runDB $ update objectiveId [ObjectiveTeamId =. team]
     returnJson objective
+
+isMemberOf :: UserId  -> TeamId -> Handler Bool
+isMemberOf userId teamId = do
+    members <- runDB $ selectList [TeamMemberUserId ==. userId, TeamMemberTeamId ==. teamId] []
+    return $ not $ null members
+
+data TeamMemberForm = TeamMemberForm
+    { teamMemberName' :: Maybe Text
+    , teamMemberDescription' :: Maybe Text
+    , teamMemberUser' :: Entity User
+    }
+
+teamMemberForm :: AForm Handler TeamMemberForm
+teamMemberForm = TeamMemberForm
+    <$> aopt textField (bfs ("Name" :: Text)) Nothing
+    <*> aopt textField (bfs ("Description" :: Text)) Nothing
+    <*> areq (selectField userOptions) (bfs ("User" :: Text)) Nothing
+    where
+        userOptions = optionsPersist [] [Asc UserIdent] userIdent
